7a6211025798ce7535ddef9e0f54b3a7c5836e83
David Chapman <davidchappy@gmail.com>
2016-11-10 09:21:54 -0300

    Add gravatars

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  def gravatar_for(user, options = { size: 80 })
+    hashed_email = Digest::MD5::hexdigest(user.email.strip.downcase)
+    gravatar_url = "https://www.gravatar.com/avatar/#{hashed_email}?s=#{options[:size]}"
+    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+  end
+
 end
