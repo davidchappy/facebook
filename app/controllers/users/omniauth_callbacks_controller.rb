c1a0cba3ce69fd8440e264dfa0a6616818fb94f0
David Chapman <davidchappy@gmail.com>
2016-11-11 11:24:28 -0300

    Experiment: hard code FB creds for Heroku - 13th try

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    @user.skip_confirmation!
+    @user.confirm
     # sign_in_and_redirect @user 
 
     if @user.persisted?
