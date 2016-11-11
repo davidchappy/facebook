00351aadbbeb84549f8860a0f58854ea7cba3e28
David Chapman <davidchappy@gmail.com>
2016-11-11 10:10:34 -0300

    Experiment: hard code FB creds for Heroku - 2nd try

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,7 +248,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV['689599474532631'], ENV['1a0329293e3fe96ed3265f673d7a08ac'], 
+  config.omniauth :facebook, '689599474532631', '1a0329293e3fe96ed3265f673d7a08ac', 
         :strategy_class => OmniAuth::Strategies::Facebook
 
   # ==> Warden configuration
