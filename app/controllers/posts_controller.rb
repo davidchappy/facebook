666d32b8cf1f7eab272840f5cdce023df8a3b21c
David Chapman <davidchappy@gmail.com>
2016-11-15 14:39:41 -0300

    Add image upload with Carrier Wave

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -51,7 +51,7 @@ class PostsController < ApplicationController
   private
 
     def post_params
-      params.require(:post).permit(:content, :user_id)
+      params.require(:post).permit(:content, :user_id, :picture)
     end
   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,17 @@ class Post < ApplicationRecord
   has_many    :comments,  dependent: :destroy
   has_many    :likes,     dependent: :destroy
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
+  validate    :picture_size
 
   validates :content, :user_id, presence: true
   validates :content, length: { minimum: 6, maximum: 5000 }
+
+  private 
+
+    def picture_size
+      if picture.size > 3.megabytes
+        errors.add(:picture, "should be less than 3MB")
+      end
+    end
 end
